0cfd644511dc4b1aa5cc589c1670276b70cdf49a
Zook1234 <59753693+NotQuiteZook@users.noreply.github.com>
2022-03-25 14:48:21 +0000

    added header

--- index.mjs
+++ index.mjs
@@ -4,7 +4,6 @@ import nodeStatic from 'node-static';
 import Analytics from './analytic.mjs';
 const bare =  new Server('/bare/', '');
 const serve = new nodeStatic.Server('static/');
-
 const server = http.createServer();
 
 server.on('request', (request, response) => {
@@ -16,6 +15,10 @@ server.on('request', (request, response) => {
 server.on('upgrade', (req, socket, head) => {
 	if(bare.route_upgrade(req, socket, head))return;
 	socket.end();
+});
+
+server.on('/games', (req, res) => {
+  res.sendFile(__dirname + '/static/games/games.html');
 });
 
 server.listen(process.env.PORT || 8080);
